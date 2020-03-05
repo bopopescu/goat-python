bfa30a6984df1ea8fc58bdbcc0c62887f5029494
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-05 15:51:01 +0800

    学习 scrapy 提交链接

--- goat/chapter7-2-3/myscrapy/myscrapy/start.py
+++ goat/chapter7-2-3/myscrapy/myscrapy/start.py
@@ -7,5 +7,6 @@ from scrapy import cmdline
 # cmdline.execute("scrapy crawl tianyancha_applets".split())
 # cmdline.execute("scrapy crawl app01".split())
 # cmdline.execute("scrapy crawl app02".split())
-cmdline.execute("scrapy crawl app03".split())
+# cmdline.execute("scrapy crawl app03".split())
+cmdline.execute("scrapy crawl app04".split())
 
